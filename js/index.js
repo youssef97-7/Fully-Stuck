0768f6681f26305fe69e0f4e66f630b00bf88cf2
hassib <youssefhassib9771@gmail.com>
2026-03-19 16:17:57 +0200

    Added navbar to all user pages and landing page, fixed FOUC bug by making a js check before the web displays anything

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 const btn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme == 'dark'){
+  document.body.classList.add('dark-mode');
+  btn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+}else {
+  btn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+}
+
 
 btn.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
+
+  if(document.body.classList.contains('dark-mode')){
+    btn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    localStorage.setItem('theme', 'dark');
+  }
+  else{
+    btn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    localStorage.setItem('theme', 'light');
+  }
 });
+
+
